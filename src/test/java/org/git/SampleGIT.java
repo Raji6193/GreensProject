f5e839e1ea3dfbf1df3eb8a2ffddff9121eb072b
Raji6193 <rajalakshmieee10@gmail.com>
2022-11-06 23:49:13 +0530

    John commit

--- src/test/java/org/git/SampleGIT.java
+++ src/test/java/org/git/SampleGIT.java
@@ -13,5 +13,17 @@ public class SampleGIT {
 	public void method3() {
 		System.out.println("Method 3");
 	}
+	
+	public void method4() {
+		System.out.println("Method 4");
+	}
+	
+	public void method5() {
+		System.out.println("Method 5");
+	}
+	
+	public void method6() {
+		System.out.println("Method 6");
+	}
 
 }
