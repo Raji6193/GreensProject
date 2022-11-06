1561dae17acc302e6223d20488d6c489ee0cff56
Raji6193 <rajalakshmieee10@gmail.com>
2022-11-06 23:25:04 +0530

    Git Practice Class

---
+++ src/test/java/org/git/SampleGIT.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class SampleGIT {
+	
+	public void method1() {
+		System.out.println("Method 1");
+	}
+	
+	public void method2() {
+		System.out.println("Method 2");
+	}
+	
+	public void method3() {
+		System.out.println("Method 3");
+	}
+
+}
